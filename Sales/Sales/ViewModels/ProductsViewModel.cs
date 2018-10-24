c0a35ac5ff703918e0b05625bab199aae0253b0f
DESKTOP-285CM7S\wilso <wilson.sanchez8303@gmail.com>
2018-10-24 11:16:50 -0500

    par1

---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Sales.ViewModels
+{
+    using Services;
+    using Sales.Common.Models;
+    using System.Collections.ObjectModel;
+    using System;
+    using Xamarin.Forms;
+    using System.Collections.Generic;
+
+    public class ProductsViewModel: BaseViewModel
+    {
+        private ApiService apiService;
+
+        private ObservableCollection<Products> products;
+
+        public ObservableCollection<Products> Products
+        {
+            get { return this.products; }
+            set { this.SetValue(ref this.products, value); }
+        }
+
+        public ProductsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            var response = await this.apiService.GetList<Products>("http://192.168.137.1/apisales", "/api", "/Products");
+            if (!response.IsSuccess) {
+                await Application.Current.MainPage.DisplayAlert("Error",response.Message,"Accept");
+                return;
+            }
+
+            var list = (List<Products>)response.Result;
+            this.Products = new ObservableCollection<Products>(list);
+
+        }
+    }
+}
